bccb3c7f5fd9c916ec38180f88cbff0327f97636
Jon <4leite@gmail.com>
2024-03-23 02:04:29 +1300

    add tohuhono site

---
+++ apps/tohuhono/src/app/layout.tsx
@@ -0,0 +1,46 @@
+import "src/app/globals.css"
+import { Montserrat } from "next/font/google"
+
+import { cn } from "@oberon/utils"
+const font = Montserrat({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "Oberon CMS",
+  description: "Built with puck by Tohuhono",
+}
+
+// Inline script to prevent fouc
+// suppressHydrationWarning added to html tag
+const ApplyMode = () => (
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+  const isDarkMode = () => {
+    if (typeof localStorage !== "undefined" && localStorage.theme === "dark") {
+      return true
+    }
+    if (typeof localStorage !== "undefined" && localStorage.theme === "light") {
+      return false
+    }
+    return window?.matchMedia("(prefers-color-scheme: dark)").matches
+  };
+  if (isDarkMode()) {
+    document.documentElement.classList.add("dark");
+  }
+  `,
+    }}
+  />
+)
+
+export default function RootLayout({ children }: React.PropsWithChildren) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <ApplyMode />
+      </head>
+      <body className={cn(font.className, "bg-background text-foreground")}>
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ apps/tohuhono/src/app/page.tsx
@@ -0,0 +1,15 @@
+export async function generateMetadata() {
+  return {
+    title: "Tohuhono ltd",
+  }
+}
+
+export default function HomePage() {
+  return (
+    <div className="flex justify-center">
+      <div className="prose">
+        <h1>Tohuhono</h1>
+      </div>
+    </div>
+  )
+}
