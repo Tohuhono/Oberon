36016f6143ea8a1cfb9eea32be08e14a89b12e0e
Jon <4leite@gmail.com>
2024-04-23 12:54:54 +1200

    Fix rich-text importing client instead of server render

--- recipes/nextjs/app/layout.tsx
+++ recipes/nextjs/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css"
 import { Noto_Sans } from "next/font/google"
 
 import { cn } from "@oberoncms/core"
-import Script from "next/script"
 const font = Noto_Sans({ subsets: ["latin"] })
 
 export const metadata = {
@@ -17,18 +16,15 @@ const ApplyMode = () => (
     dangerouslySetInnerHTML={{
       __html: `
   const isDarkMode = () => {
-    console.log('isDarkMode?')
     if (typeof localStorage !== "undefined" && localStorage.theme === "dark") {
       return true
     }
     if (typeof localStorage !== "undefined" && localStorage.theme === "light") {
       return false
     }
-    console.log('isDarkMode matchMedia')
     return window?.matchMedia("(prefers-color-scheme: dark)").matches
   };
   if (isDarkMode()) {
-    console.log('isDarkMode returned true')
     document.documentElement.classList.add("dark");
   }
   `,
@@ -40,25 +36,6 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Script id="dark-mode" strategy="beforeInteractive">
-          {`
-  const isDarkMode3 = () => {
-    console.log('isDarkMode3?')
-    if (typeof localStorage !== "undefined" && localStorage.theme === "dark") {
-      return true
-    }
-    if (typeof localStorage !== "undefined" && localStorage.theme === "light") {
-      return false
-    }
-    console.log('isDarkMode3 matchMedia')
-    return window?.matchMedia("(prefers-color-scheme: dark)").matches
-  };
-  if (isDarkMode3()) {
-    console.log('isDarkMode3 returned true')
-    document.documentElement.classList.add("dark");
-  }          
-          `}
-        </Script>
         <ApplyMode />
       </head>
       <body className={cn(font.className, "bg-background text-foreground")}>
