9da94f6b0b57e3c8a5703abf34cc2ba0e472ea81
Jon Vivian <2586037+4leite@users.noreply.github.com>
2024-05-20 01:34:10 +1200

    move playground to apps in preperation for create-oberon-app (#84)

---
+++ apps/playground/app/layout.tsx
@@ -0,0 +1,46 @@
+import "./globals.css"
+import { Noto_Sans } from "next/font/google"
+
+import { cn } from "@oberoncms/core"
+const font = Noto_Sans({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "Oberon CMS",
+  description: "Built with puck by Tohuhono",
+}
+
+// Inline script to prevent fouc
+// suppressHydrationWarning added to html tag
+const ApplyMode = () => (
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+  const isDarkMode = () => {
+    if (typeof localStorage !== "undefined" && localStorage.theme === "dark") {
+      return true
+    }
+    if (typeof localStorage !== "undefined" && localStorage.theme === "light") {
+      return false
+    }
+    return window?.matchMedia("(prefers-color-scheme: dark)").matches
+  };
+  if (isDarkMode()) {
+    document.documentElement.classList.add("dark");
+  }
+  `,
+    }}
+  />
+)
+
+export default function RootLayout({ children }: React.PropsWithChildren) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <ApplyMode />
+      </head>
+      <body className={cn(font.className, "bg-background text-foreground")}>
+        {children}
+      </body>
+    </html>
+  )
+}
