036b29f2bcd11676683f14797d44d8557379eb2b
Jon <4leite@gmail.com>
2024-06-05 15:01:52 +1200

    improve changed packages action

--- scripts/changed.sh
+++ scripts/changed.sh
@@ -1,1 +1,4 @@
-npx -y turbo build --dry-run=json --filter=...[$1]  --global-deps=.github/* | node -p 'JSON.parse(fs.readFileSync(0)).packages'
+#! /bin/bash
+npx -y turbo build --dry-run=json --filter=...[$1]  --global-deps=.github/* > changed.json
+cat changed.json
+node -pe 'JSON.stringify(JSON.parse(fs.readFileSync("./changed.json")).packages)'
