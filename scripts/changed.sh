4b74a309b826878ac3ed5526dfa89c812b257b82
Jon Vivian <2586037+4leite@users.noreply.github.com>
2024-06-05 15:48:54 +1200

    pin turbo version (#151)

--- scripts/changed.sh
+++ scripts/changed.sh
@@ -1,4 +1,2 @@
 #! /bin/bash
-npx -y turbo build --dry-run=json --filter=...[$1]  --global-deps=.github/* > changed.json
-cat changed.json
-node -pe 'JSON.stringify(JSON.parse(fs.readFileSync("./changed.json")).packages)'
+npx -y turbo@1 build --dry-run=json --filter=...[$1]  --global-deps=.github/* | node -p 'JSON.parse(fs.readFileSync("./changed.json")).packages'
