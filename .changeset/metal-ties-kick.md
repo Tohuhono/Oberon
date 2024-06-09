5a005932b813959166696e057c905eda037ada16
Jon <4leite@gmail.com>
2024-06-10 02:22:55 +1200

    Fix db client instantiating before initialisation

---
+++ .changeset/metal-ties-kick.md
@@ -0,0 +1,9 @@
+---
+"@oberoncms/plugin-vercel-postgres": patch
+"create-oberon-app": patch
+"@oberoncms/plugin-sqlite": patch
+"@oberoncms/plugin-pgsql": patch
+"@oberoncms/plugin-turso": patch
+---
+
+Fix dynamically load client after initialisation
--- packages/plugins/vercel-postgres/drizzle.config.ts
+++ packages/plugins/vercel-postgres/drizzle.config.ts
@@ -1,12 +1,7 @@
-import "./src/db/env-config"
-
 import type { Config } from "drizzle-kit"
 
 export default {
   schema: "./src/db/schema/index.ts",
   out: "./src/db/migrations",
   driver: "pg",
-  dbCredentials: {
-    connectionString: process.env.POSTGRES_URL || "",
-  },
 } satisfies Config
