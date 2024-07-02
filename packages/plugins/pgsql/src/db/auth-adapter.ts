2d10fa27e6fc20f360aa4146e8dcc73e5d3cf228
Jon <4leite@gmail.com>
2024-07-02 21:46:38 +1200

    bump drizzle

--- packages/plugins/pgsql/src/db/auth-adapter.ts
+++ packages/plugins/pgsql/src/db/auth-adapter.ts
@@ -1,6 +1,7 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import type { OberonAuthAdapter } from "@oberoncms/core"
-import { type DatabaseClient } from "./client"
+import type { DatabaseClient } from "./client"
 
-export const getAuthAdapter: (db: DatabaseClient) => OberonAuthAdapter = (db) =>
-  DrizzleAdapter(db) as OberonAuthAdapter
+export const getAuthAdapter = (db: DatabaseClient): OberonAuthAdapter =>
+  //@ts-expect-error https://github.com/drizzle-team/drizzle-orm/issues/1558
+  DrizzleAdapter(db)
