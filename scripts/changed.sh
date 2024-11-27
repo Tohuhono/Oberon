0feca7ab2d4c8c273f0f041de997d5300dba2db9
Jon <4leite@gmail.com>
2024-11-27 23:10:26 +1300

    fix turbo version

--- scripts/changed.sh
+++ scripts/changed.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-npx -y turbo@1 build --dry-run=json --filter=...[$1]  --global-deps=.github/* | node -p 'JSON.parse(fs.readFileSync(0)).packages'
+npx -y turbo build --dry-run=json --filter=...[$1]  --global-deps=.github/* | node -p 'JSON.parse(fs.readFileSync(0)).packages'
