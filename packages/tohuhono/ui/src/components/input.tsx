35bc63f2be7e5169827e46759f1e41c1a2fc406f
4leite <4leite@gmail.com>
2026-02-10 13:15:22 +1300

    prettier fix

--- packages/tohuhono/ui/src/components/input.tsx
+++ packages/tohuhono/ui/src/components/input.tsx
@@ -2,8 +2,7 @@ import * as React from "react"
 
 import { cn } from "@tohuhono/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
