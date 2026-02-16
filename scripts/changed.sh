8f23186bce5659bf16dde94c3bcc1adcf9e29dd8
4leite <4leite@gmail.com>
2026-02-16 22:41:52 +1300

    chore: migrate publishing auth to oidc

--- scripts/changed.sh
+++ scripts/changed.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-npx -y turbo build --dry-run=json --filter=...[$1]  --global-deps=.github/* | node -p 'JSON.parse(fs.readFileSync(0)).packages'
+pnpm exec turbo build --dry-run=json --filter=...[$1]  --global-deps=.github/* | node -p 'JSON.parse(fs.readFileSync(0)).packages'
