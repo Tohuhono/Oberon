6efa741dc99a4f47cea27001005701cad4d7e77a
4leite <4leite@gmail.com>
2026-03-23 01:11:13 +1300

    Use Playwright-native CI reporting

--- dev/playwright/base.config.ts
+++ dev/playwright/base.config.ts
@@ -37,7 +37,7 @@ export const base = defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : 3,
-  reporter: "line",
+  reporter: process.env.CI ? [["github"], ["html", { open: "never" }]] : "line",
   outputDir: ".playwright/results",
   use: {
     ...devices["Desktop Chrome"],
