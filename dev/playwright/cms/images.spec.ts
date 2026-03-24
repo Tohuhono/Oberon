1edf4718ca6310b8e57f8d9912a33d2b334d713b
4leite <4leite@gmail.com>
2026-03-25 00:52:39 +1300

    Restore CMS image TODO

--- dev/playwright/cms/images.spec.ts
+++ dev/playwright/cms/images.spec.ts
@@ -2,6 +2,7 @@ import { expect, test } from "@dev/playwright/helpers/fixtures"
 
 test.describe("CMS Images & Site Actions", { tag: "@cms" }, () => {
   test.skip("deletes an image from cms images page", async ({ cms }) => {
+    // TODO: seed image via CMS upload UI (requires UploadThing flow)
     await cms.goto("/cms/images")
   })
 
