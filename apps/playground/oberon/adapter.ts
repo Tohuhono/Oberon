475d67a8218740e85c3d06c1344ff871135c944a
4leite <4leite@gmail.com>
2026-04-27 17:34:51 +1200

    move pg db client to lazy init so it doesn't try to during development

--- apps/playground/oberon/adapter.ts
+++ apps/playground/oberon/adapter.ts
@@ -6,9 +6,16 @@ import { plugin as developmentPlugin } from "@oberoncms/plugin-development"
 import { plugin as tailwindPlugin } from "@oberoncms/plugin-tailwind"
 
 import { plugin as pgsqlPlugin } from "@oberoncms/plugin-pgsql"
+import { plugin as resendPlugin } from "./send"
 import { config } from "./config"
 
 export const { adapter, handler } = initOberon({
   config,
-  plugins: [developmentPlugin, pgsqlPlugin, tailwindPlugin, authPlugin],
+  plugins: [
+    developmentPlugin,
+    pgsqlPlugin,
+    resendPlugin,
+    tailwindPlugin,
+    authPlugin,
+  ],
 })
